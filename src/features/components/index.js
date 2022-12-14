dae045805963c21f854d69108a8212a901d03fc3
farah2003 <farah.shaqoura@example.com>
2022-12-14 11:40:26 +0200

    add index file

---
+++ src/features/components/index.js
@@ -0,0 +1,9 @@
+import { UpComing } from './Home/upComing';
+import { Popular } from './Home/popular';
+import { Cast } from './movieInfo/cast';
+import { Overview } from './movieInfo/aboutMovie';
+import { Reviews } from './movieInfo/reviews';
+import { NowPlaying } from './Home/nowPlaying';
+import { TopRated } from './Home/topRated';
+
+export { UpComing, Popular, Cast, Overview, Reviews, NowPlaying, TopRated };
