b9ba7c57223695fea291f4af325bca5330d4b444
farah2003 <farah.shaqoura@example.com>
2022-12-14 11:37:08 +0200

    add genaric components

---
+++ src/components/index.js
@@ -0,0 +1,48 @@
+import { SafeArea } from './utility/safeArea';
+import { MovieImage } from './utility/safeArea';
+import { CastImage } from './utility/safeArea';
+import { CastContainer } from './utility/safeArea';
+import { MovieContainer } from './utility/safeArea';
+import { Search } from './utility/safeArea';
+import { AboutMovieView } from './utility/safeArea';
+import { OverView } from './utility/safeArea';
+import { Container } from './utility/safeArea';
+import { Review } from './utility/safeArea';
+import { Avater } from './utility/safeArea';
+import { Rating } from './utility/safeArea';
+import { ReivewRightContainer } from './utility/safeArea';
+import { ReivewLeftContainer } from './utility/safeArea';
+import { Reviewer } from './utility/safeArea';
+import { Description } from './utility/safeArea';
+import { Question } from './utility/safeArea';
+import { NumberSvg } from './utility/safeArea';
+import { Suggestions } from './utility/safeArea';
+import { MovieImageWrapper } from './utility/safeArea';
+import { SuggestedMovieImage } from './utility/safeArea';
+import { Header } from './utility/safeArea';
+import { HeaderTitle } from './utility/safeArea';
+export {
+  SafeArea,
+  MovieImage,
+  CastImage,
+  MovieContainer,
+  CastContainer,
+  Search,
+  AboutMovieView,
+  OverView,
+  Review,
+  Container,
+  Avater,
+  Rating,
+  ReivewRightContainer,
+  ReivewLeftContainer,
+  Reviewer,
+  Description,
+  Question,
+  NumberSvg,
+  Suggestions,
+  MovieImageWrapper,
+  SuggestedMovieImage,
+  Header,
+  HeaderTitle,
+};
---
+++ src/components/utility/safeArea.js
@@ -0,0 +1,143 @@
+import { StatusBar, SafeAreaView, Image, View, Text } from 'react-native';
+import styled from 'styled-components/native';
+import { Svg } from 'react-native-svg';
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  background-color: #242a32;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+export const Container = styled(View)`
+  flex: 1;
+  background-color: #242a32;
+`;
+export const MovieContainer = styled(View)`
+  margin-horizontal: 4px;
+  margin-top: 20px;
+`;
+
+export const CastContainer = styled(View)`
+  margin-horizontal: 40px;
+  margin-top: 20px;
+`;
+
+export const MovieImage = styled(Image)`
+  border-radius: 15;
+  width: 120px;
+  height: 170px;
+  marginbottom: 10px;
+`;
+
+export const CastImage = styled(Image)`
+  border-radius: 50px;
+  width: 100px;
+  height: 100px;
+  marginbottom: 10px;
+`;
+
+export const Search = styled(View)`
+  background-color: #3a3f47;
+  width: 90%;
+  height: 45;
+  margin-horizontal: 20;
+  margin-bottom: 10;
+  border-radius: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-horizontal: 10;
+`;
+export const AboutMovieView = styled(View)`
+  flex: 1;
+  background-color: #242a32;
+  padding: 29px;
+`;
+export const OverView = styled(Text)`
+  font-size: 14;
+  font-weight: 400;
+  line-height: 18;
+  color: #ffff;
+  align-items: center;
+`;
+
+export const Review = styled(View)`
+  flex-direction: row;
+  padding: 24px;
+`;
+export const Avater = styled(Image)`
+  width: 50;
+  height: 50;
+  border-radius: 50;
+`;
+export const Rating = styled(Text)`
+  font-size: 14;
+  font-weight: 500;
+  color: #0296e5;
+  align-items: center;
+  text-align: center;
+  margin-top: 10;
+`;
+
+export const ReivewRightContainer = styled(View)`
+  width: 15%;
+  margin-right: 10px;
+`;
+export const ReivewLeftContainer = styled(View)`
+  width: 83%;
+`;
+export const Reviewer = styled(Text)`
+  font-size: 13;
+  color: #ffff;
+  line-height: 18;
+  font-weight: 700;
+`;
+
+export const Description = styled(Text)`
+  font-size: 13;
+  color: #ffff;
+  line-height: 18;
+  font-weight: 400;
+`;
+
+export const Question = styled(Text)`
+  font-size: 18;
+  color: #ffff;
+  line-height: 27;
+  font-weight: 700;
+  margin-horizontal: 20;
+  marign-vertical: 12;
+`;
+
+export const NumberSvg = styled(Svg)`
+  position: absolute;
+  right: 40;
+  top: 150;
+`;
+export const Suggestions = styled(View)`
+  flex: 0.63;
+`;
+export const MovieImageWrapper = styled(View)`
+  padding: 5px;
+  margin-horizontal: 12;
+`;
+export const SuggestedMovieImage = styled(Image)`
+  width: 145;
+  height: 210;
+  border-radius: 15;
+`;
+export const Header = styled(View)`
+  flex-direction: row;
+  padding-horizontal: 24;
+  flex: 0.1;
+  align-content: center;
+  align-items: center;
+  margin-vertical: 10;
+`;
+
+export const HeaderTitle = styled(Text)`
+  font-size: 18;
+  line-height: 20;
+  font-weight: 600;
+  color: #ffffff;
+  margin-horizontal: 100;
+`;
